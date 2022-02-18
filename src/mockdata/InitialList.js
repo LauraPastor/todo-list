39d5e00df942992f80f398d29cc9ee6c6feeeaac
LauraPastor <laurapastorj@gmail.com>
2022-02-18 14:27:38 +0100

    Add MockData

--- src/mockdata/InitialList.js
+++ src/mockdata/InitialList.js
@@ -1,16 +1,16 @@
 export default [
     {
       id: 1,
-      title: "Uivar",
+      title: "Take Pancho for a walk",
       isCompleted: false,
     },
     {
       id: 2,
-      title: "Code",
+      title: "Go to Bolu to buy tomatoes",
       isCompleted: false,
     },
     {
       id: 3,
-      title: "Zsomething",
+      title: "Meet Noel at Treptower Park",
       isCompleted: false,
     },]
